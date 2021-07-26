24e19a1b035b303870abb9eebf6393b657f8b49a
Ranji-Kanatani <ranji_kanatani@hulft.com>
2021-07-26 17:31:37 +0900

    remove()を追加

--- src/com/app/Tasks.java
+++ src/com/app/Tasks.java
@@ -1,4 +1,13 @@
 package com.app;
 
+import java.util.ArrayList;
+
 public class Tasks {
+    ArrayList<Task> tasks = new ArrayList<Task>();
+    public Tasks(Task task){
+        tasks.add(task); //二次元にせなあかんな～
+    }
+    public void Remove(int num){
+        tasks.remove(num);
+    }
 }
